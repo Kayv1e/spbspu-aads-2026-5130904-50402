8db9875145a46cb16edc74464c6752c175ea6389
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-08 16:10:52 +0300

    Basic HashTable implementation

--- novikov.ivan/S3/HashTable.hpp
+++ novikov.ivan/S3/HashTable.hpp
@@ -1,16 +1,83 @@
 #ifndef HASHTABLE_HPP
 #define HASHTABLE_HPP
 #include <cstddef>
+#include <utility>
 
 namespace novikov
 {
   template < class Key, class Value, class Hash, class Equal >
-  class HashTable {
+  class HashTable;
+
+  template < class Key, class Value, class Hash, class Equal >
+  class HTIter
+  {
   public:
-    void add(Key k, Value v);
-    Value drop(Key k);
-    bool has(Key k);
+    explicit HTIter(HashTable< Key, Value, Hash, Equal >&);
+    bool operator==(const HTIter&) const noexcept;
+    bool operator!=(const HTIter&) const noexcept;
+    HTIter& operator++();
+    HTIter& operator--();
+    std::pair< Key, Value >& operator*();
+
+  private:
+    size_t pos_;
+    HashTable< Key, Value, Hash, Equal >& table_;
+  };
+
+  template < class Key, class Value, class Hash, class Equal >
+  class HTCIter
+  {
+  public:
+    explicit HTCIter(const HashTable< Key, Value, Hash, Equal >&);
+    bool operator==(const HTCIter&) const noexcept;
+    bool operator!=(const HTCIter&) const noexcept;
+    HTCIter& operator++();
+    HTCIter& operator--();
+    const std::pair< Key, Value >& operator*();
+
+  private:
+    size_t pos_;
+    const HashTable< Key, Value, Hash, Equal >& table_;
+  };
+
+  template < class Key, class Value, class Hash, class Equal >
+  class HashTable
+  {
+  public:
+    HashTable();
+    HashTable(const HashTable& rhs);
+    HashTable(HashTable&& rhs);
+    ~HashTable();
+    HashTable& operator=(const HashTable& rhs);
+    HashTable& operator=(HashTable&& rhs);
+    Value& operator[](Key key) noexcept;
+
+    HTIter< Key, Value, Hash, Equal > begin();
+    HTCIter< Key, Value, Hash, Equal > begin() const;
+    HTCIter< Key, Value, Hash, Equal > cbegin() const;
+
+    HTIter< Key, Value, Hash, Equal > end();
+    HTCIter< Key, Value, Hash, Equal > end() const;
+    HTCIter< Key, Value, Hash, Equal > cend() const;
+
+    void insert(Key k, Value v);
+    void erase(Key k);
+    bool contains(Key k) const;
     void rehash(size_t slots);
+    bool empty() const;
+    size_t slotsCount() const;
+    size_t size() const;
+    Value& at(Key key);
+
+  private:
+    struct Slot
+    {
+      Key key;
+      Value value;
+      bool isEmpty;
+    };
+    size_t size_;
+    Slot* slots_;
   };
 }
 
