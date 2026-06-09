4a4e5d70359a4d2b7022b1b34cb70387f94bb6ca
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-09 15:25:35 +0300

    Remade hasher

--- novikov.ivan/S3/hasher.hpp
+++ novikov.ivan/S3/hasher.hpp
@@ -1,50 +1,45 @@
 #ifndef HASHER_HPP
 #define HASHER_HPP
-#include <array>
-#include <cstddef>
-#include <random>
-#include <boost/hash2/siphash.hpp>
-#include <boost/hash2/hash_append.hpp>
 #include <boost/hash2/get_integral_result.hpp>
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/siphash.hpp>
+#include <random>
+#include <array>
 
 namespace novikov
 {
-  template <class Key>
+  template< class T >
   class SipHash
   {
-    boost::hash2::siphash_64 h_;
-    static std::array<unsigned char, 16> make_seed_16()
-    {
-      std::array<unsigned char, 16> s{};
-      std::random_device rd;
-
-      for (auto& b : s)
-        b = static_cast<unsigned char>(rd());
-
-      return s;
-    }
-
   public:
-    SipHash()
-    {
-      auto seed = make_seed_16();
-      h_ = boost::hash2::siphash_64(seed.data(), seed.size());
-    }
+    SipHash();
+    size_t operator()(const T& v) const;
 
-    explicit SipHash(std::array<unsigned char, 16> const& seed)
-        : h_(seed.data(), seed.size())
-    {
-    }
-
-    SipHash(unsigned char const *p, std::size_t n) : h_(p, n) {}
-
-    std::size_t operator()(Key const &key) const
-    {
-      auto h = h_;
-      boost::hash2::hash_append(h, {}, key);
-      return boost::hash2::get_integral_result<std::size_t>(h);
-    }
+  private:
+    boost::hash2::siphash_64 h_;
   };
+}
+
+template< class T >
+novikov::SipHash< T >::SipHash()
+{
+  std::random_device rd;
+  std::array< unsigned char, 16 > random_seed;
+  
+  for (size_t i = 0; i < random_seed.size(); ++i)
+  {
+    random_seed[i] = static_cast< unsigned char >(rd());
+  }
+  
+  h_ = boost::hash2::siphash_64(random_seed);
+}
+
+template< class T >
+size_t novikov::SipHash< T >::operator()(const T& v) const
+{
+  boost::hash2::siphash_64 h(h_);
+  boost::hash2::hash_append(h, boost::hash2::default_flavor{}, v);
+  return boost::hash2::get_integral_result< size_t >(h);
 }
 
 #endif
