42a6964a893a4badb1b7318fdff331c405289159
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-13 14:52:42 +0300

    Implemented most of the BSTree

--- novikov.ivan/S4/BSTree.hpp
+++ novikov.ivan/S4/BSTree.hpp
@@ -1,6 +1,19 @@
 #ifndef BSTREE_HPP
 #define BSTREE_HPP
+#include <iostream>
 #include <cstddef>
+#include <stdexcept>
+#include <utility>
+#include <bilist.hpp>
+
+namespace novikov
+{
+  template< class Key, class Value, class Compare >
+  class BSTree;
+}
+
+using BSTList = novikov::List< novikov::BSTree< std::string, size_t, std::less< std::string > > >;
+using cmd_t = void (*)(std::istream& in, BSTList);
 
 namespace novikov
 {
@@ -18,9 +31,17 @@ namespace novikov
   class BSTree {
 
   public:
+    BSTree();
+    ~BSTree();
+    BSTree(const BSTree& other);
+    BSTree(BSTree&& other) noexcept;
+    BSTree& operator=(BSTree other);
+    Value& operator[](const Key& k);
+
     void push(Key k, Value v);
-    Value get(Key k) const;
-    void drop(Key k);
+    const Value& get(const Key& k) const;
+    void drop(const Key& k);
+    bool has(const Key& k) const;
 
     using const_iterator = BSTConstIterator< Key, Value >;
     const_iterator rotateLeft(const_iterator it);
@@ -36,12 +57,254 @@ namespace novikov
     {
       Key key_;
       Value value_;
-      Node *left_ = nullptr;
-      Node *right_ = nullptr;
+      Node* left_ = nullptr;
+      Node* right_ = nullptr;
     };
-    Node *fakeroot_;
+    Node* fakeroot_;
     Compare compare_;
+
+    void clear(Node* fakeroot);
+    Node* copyNodes(Node* other);
+    void swap(BSTree& other) noexcept;
+    Value& insert(Key k, Value v, bool flag);
+    Node* fallLeft(Node* node);
   };
+
+  void print(std::istream& in, BSTList);
+  void complement(std::istream& in, BSTList);
+  void intersect(std::istream& in, BSTList);
+  void union_(std::istream& in, BSTList);
+}
+
+template< class Key, class Value, class Compare >
+novikov::BSTree<Key, Value, Compare>::BSTree():
+    fakeroot_(new Node{Key(), Value()}),
+    compare_(Compare())
+{}
+
+template< class Key, class Value, class Compare >
+novikov::BSTree<Key, Value, Compare>::~BSTree()
+{
+  clear(fakeroot_);
+}
+
+template< class Key, class Value, class Compare >
+novikov::BSTree<Key, Value, Compare>::BSTree(const BSTree& other)
+{
+  fakeroot_ = new Node();
+  fakeroot_->left_ = copyNodes(other.fakeroot_->left_);
+  compare_ = other.compare_;
+}
+
+template< class Key, class Value, class Compare >
+novikov::BSTree<Key, Value, Compare>::BSTree(BSTree&& other) noexcept:
+    BSTree()
+{
+  swap(other);
+}
+
+template< class Key, class Value, class Compare >
+novikov::BSTree<Key, Value, Compare>& novikov::BSTree<Key, Value, Compare>::operator=(BSTree other)
+{
+  swap(other);
+  return *this;
+}
+
+template< class Key, class Value, class Compare >
+Value& novikov::BSTree<Key, Value, Compare>::insert(Key k, Value v, bool isOperator)
+{
+  Node* curr = fakeroot_;
+  Node* next = fakeroot_->left_;
+  while (next)
+  {
+    curr = next;
+    if (compare_(k, curr->key_))
+    {
+      next = curr->left_;
+    }
+    else if (compare_(curr->key_, k))
+    {
+      next = curr->right_;
+    }
+    else
+    {
+      if (isOperator)
+      {
+        return curr->value_;
+      }
+      else
+      {
+        throw std::invalid_argument("Value already exists");
+      }
+    }
+  }
+  Node* newNode = new Node;
+  newNode->key_ = k;
+  newNode->value_ = v;
+  if (curr == fakeroot_ || compare_(k, curr->key_))
+  {
+    curr->left_ = newNode;
+  }
+  else
+  {
+    curr->right_ = newNode;
+  }
+  return newNode->value_;
+}
+
+template< class Key, class Value, class Compare >
+Value& novikov::BSTree<Key, Value, Compare>::operator[](const Key& k)
+{
+  return insert(k, Value(), true);
+}
+
+template< class Key, class Value, class Compare >
+void novikov::BSTree<Key, Value, Compare>::clear(Node* root)
+{
+  if (!root)
+  {
+    return;
+  }
+  clear(root->left_);
+  clear(root->right_);
+  delete root;
+}
+
+template< class Key, class Value, class Compare >
+typename novikov::BSTree<Key, Value, Compare>::Node*
+    novikov::BSTree<Key, Value, Compare>::copyNodes(Node* other)
+{
+  if (!other)
+  {
+    return nullptr;
+  }
+  Node* newBSTree = new Node;
+  newBSTree->key_ = other->key_;
+  newBSTree->value_ = other->value_;
+  newBSTree->left_ = copyNodes(other->left_);
+  newBSTree->right_ = copyNodes(other->right_);
+  return newBSTree;
+}
+
+template< class Key, class Value, class Compare >
+void novikov::BSTree<Key, Value, Compare>::swap(BSTree& other) noexcept
+{
+  std::swap(fakeroot_, other.fakeroot_);
+  std::swap(compare_, other.compare_);
+}
+
+template< class Key, class Value, class Compare >
+void novikov::BSTree<Key, Value, Compare>::push(Key k, Value v)
+{
+  insert(k, v, false);
+}
+
+template< class Key, class Value, class Compare >
+const Value& novikov::BSTree<Key, Value, Compare>::get(const Key& k) const
+{
+  Node* curr = fakeroot_->left_;
+  while (curr)
+  {
+    if (compare_(k, curr->key_))
+    {
+      curr = curr->left_;
+    }
+    else if (compare_(curr->key_, k))
+    {
+      curr = curr->right_;
+    }
+    else
+    {
+      return curr->value_;
+    }
+  }
+  throw std::out_of_range("Key does not exist");
+}
+
+template< class Key, class Value, class Compare >
+void novikov::BSTree<Key, Value, Compare>::drop(const Key& k)
+{
+  Node* parent = fakeroot_;
+  Node* curr = fakeroot_->left_;
+
+  while (curr)
+  {
+    if (compare_(k, curr->key_))
+    {
+      parent = curr;
+      curr = curr->left_;
+    }
+    else if (compare_(curr->key_, k))
+    {
+      parent = curr;
+      curr = curr->right_;
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  if (!curr)
+  {
+    throw std::out_of_range("Key does not exist");
+  }
+
+  if (curr->left_ && curr->right_)
+  {
+    Node *newCurr = fallLeft(curr->right_);
+    Node *newCurrParent = curr;
+    if (newCurrParent->right_ != newCurr)
+    {
+      newCurrParent = curr->right_;
+      while (newCurrParent->left_ != newCurr)
+      {
+        newCurrParent = newCurrParent->left_;
+      }
+    }
+
+    curr->key_ = newCurr->key_;
+    curr->value_ = newCurr->value_;
+    parent = newCurrParent;
+    curr = newCurr;
+  }
+
+  Node* child;
+
+  if (curr->left_)
+  {
+    child = curr->left_;
+  }
+  else
+  {
+    child = curr->right_;
+  }
+
+  if (parent->left_ == curr)
+  {
+    parent->left_ = child;
+  }
+  else
+  {
+    parent->right_ = child;
+  }
+
+  delete curr;
+}
+
+template< class Key, class Value, class Compare >
+typename novikov::BSTree<Key, Value, Compare>::Node*
+    novikov::BSTree<Key, Value, Compare>::fallLeft(Node* node)
+{
+  if (!node)
+  {
+    return node;
+  }
+  while(node->left_)
+  {
+    node = node->left_;
+  }
+  return node;
 }
 
 #endif
