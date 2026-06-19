a03f4f6dc856bdfa206d7ef80342ebca9f7c5e58
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-19 20:24:01 +0300

    Implemented main function and added process and snapshot structures

--- novikov.ivan/F0/main.cpp
+++ novikov.ivan/F0/main.cpp
@@ -1,2 +1,55 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <map>
+#include "AVLTree.hpp"
+#include "commands.hpp"
+#include "ProcessInfo.hpp"
+
 int main()
-{}
+{
+  std::map< std::string, novikov::ProcessSnapTree > snapshots;
+
+  using cmd_t = void (*)(std::istream& in, std::ostream& out, std::map< std::string, novikov::ProcessSnapTree >& snaps);
+
+  novikov::AVLTree< std::string, cmd_t, std::less< std::string > > cmds;
+
+  cmds["snap"] = novikov::snap;
+  cmds["displayAllProcesses"] = novikov::displayAllProcesses;
+  cmds["getProcessDetails"] = novikov::getProcessDetails;
+  cmds["compareSnapshots"] = novikov::compareSnapshots;
+  cmds["filterRAM"] = novikov::filterRAM;
+  cmds["filterCPU"] = novikov::filterCPU;
+  cmds["exportSnapshot"] = novikov::exportSnapshot;
+  cmds["list"] = novikov::list;
+  cmds["clearAll"] = novikov::clearAll;
+  cmds["help"] = novikov::help;
+
+  std::string cmd;
+
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      if (!cmds.has(cmd))
+      {
+        throw std::logic_error("Invalid command");
+      }
+      cmds[cmd](std::cin, std::cout, snapshots);
+    }
+    catch (const std::exception&)
+    {
+      std::cin.clear();
+      auto toIgnore = std::numeric_limits< std::streamsize >::max();
+      std::cin.ignore(toIgnore, '\n');
+    }
+  }
+
+  if (!std::cin.eof())
+  {
+    std::cerr << "Invalid input\n";
+    return 1;
+  }
+
+  return 0;
+}
