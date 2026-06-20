af1486c499dc9b99446896fea2e806da9f58a3d9
Kayv1e <128278935+Kayv1e@users.noreply.github.com>
2026-06-20 21:52:45 +0300

    Implemented displayAllProcesses

--- novikov.ivan/F0/commands.cpp
+++ novikov.ivan/F0/commands.cpp
@@ -24,4 +24,35 @@ namespace novikov
 
     out << "Snapshot " << snap_name << " has been created.\n";
   }
+
+  void displayAllProcesses(std::istream& in, std::ostream& out, novikov::SnapshotTree& snaps)
+  {
+    std::string snap_name;
+    if (!(in >> snap_name)) return;
+
+    if (!snaps.has(snap_name))
+    {
+      out << "<SNAPSHOT NOT FOUND>\n";
+      throw std::runtime_error("Snapshot not found");
+    }
+
+    Snapshot* snap = snaps.get(snap_name);
+    ProcessSnapTree& tree = snap->processTree;
+
+    for (auto it = tree.begin(); it != tree.end(); ++it)
+    {
+      out << toUtf8((*it).first) << " (PIDs: ";
+
+      const auto& procs = (*it).second;
+      for (size_t i = 0; i < procs.size(); ++i)
+      {
+        out << procs[i].pid;
+        if (i + 1 < procs.size())
+        {
+          out << ", ";
+        }
+      }
+      out << ")\n";
+    }
+  }
 }
